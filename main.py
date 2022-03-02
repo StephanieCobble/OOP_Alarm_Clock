d8aa6d861b6df112d6647dade3386e97b4d7385a
StephanieCobble <cobble.stephanie@gmail.com>
2022-03-02 11:44:31 -0800

    updated file name and tested False for alarm status

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from Alarm_Clock import AlarmClock
 
-my_alarm_clock = AlarmClock(True) 
+my_alarm_clock = AlarmClock(False) 
 my_alarm_clock.set_current_time()   #REMEMBER THE ()!!!!!!!!!!!!
 my_alarm_clock.alarm_on_off()
 my_alarm_clock.set_alarm_time()
